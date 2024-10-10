4bfd8190003a48ec00f8a1dd56303aa76296194c
vivekachari29 <vivekachari2@gmail.com>
2024-10-10 19:55:42 +0900

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 
-app.listen(4000, () => {
-  console.log(`Server listening at port 4000`);
+const port = process.env.PORT || 4000;  // Use the PORT environment variable or fallback to 4000
+
+app.listen(port, () => {
+  console.log(`Server listening at port ${port}`); // Log the port number
 });
