b0a5d230dfd3c24a18cd746975780c1bc73b33e6
vivekachari29 <vivekachari2@gmail.com>
2024-10-20 03:34:52 +0900

    Update Contact.jsx

--- front-end/src/components/Contact.jsx
+++ front-end/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://mern-stack-event-planning-project-6p6y.onrender.com/api/v1/message/send",
         {
           name,
           email,
